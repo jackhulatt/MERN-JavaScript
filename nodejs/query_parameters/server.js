ead035628be480f272e24fb3602730359858923b
MrWalshyType2 <mwalsh@qa.com>
2022-08-24 11:53:10 +0100

    Added query parameter example

--- nodejs/query_parameters/server.js
+++ nodejs/query_parameters/server.js
@@ -1,14 +1,18 @@
-const { info } = require("console");
 const http = require("http");
 const URL = require('url');
 
 const HOST = "localhost"; // 127.0.0.1 is also valid (local loopback address), it's a special reserved IPv4 address
 const PORT = 3000;
 
-const server = http.createServer(requestHandler);
-
 function requestHandler(request, response) {
-  const url = URL.parse(request.url, true);
+    // URL.parse will return an object with the different parts of the URL
+    // on it
+    // - url.pathname will be everything before any query parameters in the URL but after the
+    //   host and port address
+    // parse(urlToParse, parseQueryParamsOrNot)
+  const url = URL.parse(request.url, true); // true will also parse the query parameters
+  // false means the query parameters are not parsed
+  // query parameters in a URL: localhost:3000/somepath?queryParam1=value&queryParam2=value2
 
   switch (url.pathname) {
     case "/":
@@ -26,7 +30,10 @@ function requestHandler(request, response) {
 function infoPage(request, response) {
     const url = URL.parse(request.url, true);
 
+    // when query parameters are parsed, they are put in the url object on its
+    // query property
     const country = url.query.country;
+    const city = url.query.city;
     
     response.statusCode = 200;
     
@@ -35,8 +42,8 @@ function infoPage(request, response) {
 
     // write some data to the response body
     response.write("<html>");
-    response.write("<head><title>Site | Home</title></head>");
-    response.write(`<body><p>The country is ${country}</p></body>`);
+    response.write("<head><title>Site | Info</title></head>");
+    response.write(`<body><p>The country is ${country} and the city is ${city}</p></body>`);
     response.write("</html>");
     response.end();
 }
@@ -73,5 +80,6 @@ function notFound(request, response) {
   response.end();
 }
 
+const server = http.createServer(requestHandler);
 // start the server by calling .listen() and passing a port, host address and callback to run when the server starts
 server.listen(PORT, HOST, () => console.log(`Server up on ${HOST}:${PORT}`));
